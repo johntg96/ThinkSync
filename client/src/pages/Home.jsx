f8a9d1c4dc9b8eb9180f6f1e0887054c8b137ea0
user <user@host.lan>
2024-08-05 05:54:20 -0500

    added how-to information to "Home" page. Added new accent color (orange) and applied animation to decision button

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -36,9 +36,37 @@ export default function Home(props) {
         <MDBCol md='12'>
           <MDBCard alignment='center' className='bg-dark text-white'>
           <MDBCardImage src={ groupPhoto } position='top' alt='...' />
-            <MDBCardHeader  tag="h2">About</MDBCardHeader>
+            <MDBCardHeader tag="h2" className='pt-4 pb-2'>About</MDBCardHeader>
+            <MDBCardBody className='mt-2'>
+              <p><em>ThinkSync is a dynamic online platform designed to enhance collaboration and productivity among individuals and teams. Whether you're a student, professional, or creative thinker, ThinkSync provides a virtual space for brainstorming, organizing ideas, and working together seamlessly.</em></p>
+            </MDBCardBody>
+          </MDBCard>
+          <MDBCard alignment='left' className='bg-dark text-white mt-2'>
             <MDBCardBody>
-              <p>ThinkSync is a dynamic online platform designed to enhance collaboration and productivity among individuals and teams. Whether you're a student, professional, or creative thinker, ThinkSync provides a virtual space for brainstorming, organizing ideas, and working together seamlessly.</p>
+              <h4>How to use this tool:</h4>
+              <br/>
+              <p>
+                <ol>
+                  <li>Navigate to the "New Think" button to get to the "Think" page.</li>
+                  <li>Type in your main question or subject into the question field on the "Think" page.</li>
+                  <li>Fill in the idea box with one of your ideas (a possible solution or maybe a general idea), click the "+" button as you go.</li>
+                  <li>Click the "Submit" button to continue.</li>
+                  <li>This will bring you to the "Sync" page.</li>
+                </ol>
+              </p>
+              <br/>
+              <h4>About the Sync page:</h4>
+              <br/>
+              <p>
+              After you have submitted your brainstorming data you will be provided with several "Decision Cards". These are your ideas you submitted, shortened (if too long), shuffled, tossed, mixed, paired and ready for you to make (somewhat easier) decisions.
+              The point of doing this activity is to lay all your ideas out on (digital) paper. 
+              <br/>
+              <br/>
+              The mixing/shuffling/pairing aspect reduces bias in the decision making process.
+              <br/>
+              <br/>
+              You will come to a completion point where and then given data about your decision making process. This is referred to as a completed "Think".
+              </p>
             </MDBCardBody>
           </MDBCard>
         </MDBCol>
